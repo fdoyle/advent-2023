cb776a14f601ff2f42e544746463efcd6d9a65f8
frankd <fdoyl001@gmail.com>
2023-12-03 23:51:57 -0600

    add day4

--- day3/app/Main.hs
+++ day3/app/Main.hs
@@ -1,4 +1,21 @@
 module Main where
+import Data.List.Split (splitOn)
+import System.IO
+
+
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do 
+    putStrLn "AoC day 3"
+
+    handle <- openFile "input.txt" ReadMode
+    contents <- hGetContents handle
+    let fileLines = lines contents
+    let candidates = map getCandidateNumbers fileLines
+    print candidates
+
+
+getCandidateNumbers :: [Char] -> [String]
+getCandidateNumbers s = 
+    let list = splitOn "."  s
+    in filter (/= "") list
